d380e3e3696fc141a4da6f5f669da0ce53fbc03a
shivam <shivam@192.168.1.3>
2025-10-04 22:20:02 +0530

    added new file for our data

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000, '0.0.0.0');
+  console.log("How are you????")
 }
 bootstrap();
+
+
